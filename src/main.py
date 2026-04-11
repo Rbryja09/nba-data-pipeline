74696c98bb86c41e6b5aed3c9b5299bcd2cd6ae8
Rbryja09 <radek.bryja@gmail.com>
2026-04-11 12:03:00 -0500

    Vim setup + filtering for active players

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
+from nba_api.stats.static import players
 import pandas as pd
 import numpy as np
+
+df = pd.DataFrame(players.get_players())
+
+filtered_for_active = df[df["is_active"]]
+print(filtered_for_active.columns)
+
 
