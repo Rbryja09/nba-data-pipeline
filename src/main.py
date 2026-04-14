8c18d2a8402d6b0fa2934682848434a8af1b7842
Rbryja09 <radek.bryja@gmail.com>
2026-04-13 20:40:18 -0500

    Grabbing NBA stats data from nba-api

--- src/main.py
+++ src/main.py
@@ -1,10 +1,45 @@
-from nba_api.stats.static import players
-import pandas as pd
 import numpy as np
+import pandas as pd
+from nba_api.stats.endpoints import leaguedashplayerstats, commonteamroster
+from nba_api.stats.static import teams
 
-df = pd.DataFrame(players.get_players())
+def player_nba_stats():
+    player_stats = leaguedashplayerstats.LeagueDashPlayerStats(
+        season='2025-26',
+        per_mode_detailed='Totals',
+        height_nullable=True
+    )
 
-filtered_for_active = df[df["is_active"]]
-print(filtered_for_active.columns)
+    df = player_stats.get_data_frames()[0]
+    return df.columns
 
 
+def player_physical_stats():
+    team_list = []
+    nba_teams = teams.get_teams()
+   
+    for team in nba_teams:
+    
+        player_stats = commonteamroster.CommonTeamRoster(
+            season='2025-26',
+            team_id = team['id']
+        )
+        
+        df = player_stats.get_data_frames()[0]
+    
+
+
+    return team_list
+
+
+def average_nba_height():
+    df = common_player_stats()
+    avg_height = np.mean(df['height'])    
+    return avg_height
+
+
+if __name__ == '__main__':
+    print(player_physical_stats())
+
+
+    
